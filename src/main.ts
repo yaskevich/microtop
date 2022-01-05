92415a35ce1eb8efc416eb374d82a35e0df011d0
yaskevich <alyaxey.yaskevich@gmail.com>
2022-01-05 19:41:58 +0300

    init app

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import {createApp} from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+const app = createApp(App);
+app.component('Button', Button);
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+// import 'primeicons/primeicons.css';   
+
+app
+.use(PrimeVue)
+.mount('#app');
+
