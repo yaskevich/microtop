20e52bc97d77d670418881846e47182bd735677d
yaskevich <alyaxey.yaskevich@gmail.com>
2022-01-10 00:10:07 +0300

    enable primeflex

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import Button from 'primevue/button';
 const app = createApp(App);
 app.component('Button', Button);
 
+import 'primeflex/primeflex.css';
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
-// import 'primeicons/primeicons.css';   
+// import 'primeicons/primeicons.css';
 
 app
 .use(PrimeVue)
 .mount('#app');
-
